9fcda5d0d67c11c755fdfa689a2de9d28089fc44
Alexandru N. Onea <onea.alex@gmail.com>
2021-12-30 19:31:28 +0200

    collection: models: initial wip model

--- src/collection/models.py
+++ src/collection/models.py
@@ -1,1 +1,30 @@
 from django.db import models
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class MusicalText(models.Model):
+
+    CONTRIBUTION_TYPES = [
+        ('C', "Composed"),
+        ('T', "Translated"),
+        ('P', "Processed"),
+    ]
+
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Person)
+
+    original = models.ForeignKey(
+        'self',
+        blank=True
+    )
+
+    contribution = models.CharField(
+        max_length=1,
+        choices=CONTRIBUTION_TYPES,
+        default='C'
+    )
+
+    notes = models.CharField(max_length=512)
