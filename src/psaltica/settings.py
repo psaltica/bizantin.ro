54705bc91ec56c7f2e865153a9427ada27482679
Alexandru N. Onea <alexandru@fratiionea.ro>
2022-01-03 23:15:20 +0200

    views: initial index and nav

--- src/psaltica/settings.py
+++ src/psaltica/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'psaltica.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'psaltica/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
