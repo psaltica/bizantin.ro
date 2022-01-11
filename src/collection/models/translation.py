fbf2bd9edadac2f6e4653de1b049f32acffe43b0
Alexandru N. Onea <onea.alex@gmail.com>
2022-01-11 17:27:11 +0200

    models: improve validation for translation

--- src/collection/models/translation.py
+++ src/collection/models/translation.py
@@ -4,18 +4,17 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ValidationError
 from django.db import models
+
 from functools import reduce
 
 
 class Translation(CollectionModel):
-    def _get_translatable_model_choices():
-        translatable_models = TranslatableModel.__subclasses__()
-        expressions = [
-            models.Q(app_label='collection', model=cls.__name__.lower())
-            for cls in translatable_models
-        ]
-
-        return reduce(lambda a, b: a | b, expressions)
+    TRANSLATABLE_MODELS = reduce(lambda a, b: a | b, [
+        models.Q(
+            app_label='collection',
+            model=cls.__name__.lower()
+        ) for cls in TranslatableModel.__subclasses__()
+    ])
 
     lang = models.ForeignKey(
         Language,
@@ -29,12 +28,15 @@ class Translation(CollectionModel):
     original_type = models.ForeignKey(
         ContentType,
         on_delete=models.CASCADE,
-        limit_choices_to=_get_translatable_model_choices(),
+        limit_choices_to=TRANSLATABLE_MODELS
     )
 
     field = models.CharField(max_length=32)
 
     def clean(self):
+        if not self.original.__class__ in TranslatableModel.__subclasses__():
+            raise ValidationError('Translation refers to non-translatable original')
+
         if self.original.lang == self.lang:
             raise ValidationError('Cannot translate to the same lang as the original.')
 
