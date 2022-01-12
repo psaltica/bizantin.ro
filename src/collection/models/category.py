0403d452f85c08b43ac04959f1351a0dcd7c4c07
Alexandru N. Onea <alexandru@fratiionea.ro>
2022-01-13 00:09:59 +0200

    models: add category methods for base and leaf categories

--- src/collection/models/category.py
+++ src/collection/models/category.py
@@ -9,6 +9,20 @@ class Category(CollectionModel, TranslatableModel):
     )
 
     parent = models.ForeignKey(
-        'Self',
-        on_delete=models.CASCADE
+        'self',
+        on_delete=models.CASCADE,
+        null=True
     )
+
+    def is_base_category(self):
+        return self.parent_id is None
+
+    def get_base_category(self):
+        c = self
+        while not c.is_base_category():
+            c = c.parent
+
+        return c.name
+
+    def is_leaf_category(self):
+        return not Category.objects.filter(parent=self.id).exists()
